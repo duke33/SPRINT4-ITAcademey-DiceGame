0c883a19d5e58c6ae13c453df751cbe0bc1c59aa
Mariano <mariano_farace@hotmail.com>
2021-12-08 20:07:30 +0100

    Remove todo's

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   MONGODB_URI,
   PORT,
 };
-// TODO aplicar la uri de mongo
+
 // TODO fijate de subir .env
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const config = require('./config');
 
 const server = http.createServer(app);
 
-// TODO acomodar porque el port no sale aca!
 server.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
