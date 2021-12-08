dc17b848b25cc2983a65388f8b8047bc97103f9a
Mariano <mariano_farace@hotmail.com>
2021-12-08 17:57:46 +0100

    Delete useless file

--- test.js
+++
@@ -1,9 +0,0 @@
-User.findByIdAndUpdate(playerId, { $push: { gameLog: result }, $set: { successRate: findSimilarTypes() } }, (err, noSeQUe) => {
-  if (err) {
-    res.send(err);
-  } else {
-    noSeQUe.findSimilarTypes();
-    res.send(result);
-  }
-});
-});
