ac5a06ab2499b58e02acada84f6f6d886fcb9b00
Mariano <mariano_farace@hotmail.com>
2021-12-08 23:22:39 +0100

    Change log message

--- app/data-base/db-mongo.js
+++ app/data-base/db-mongo.js
@@ -4,5 +4,5 @@ const config = require('../../config');
 const mongoDb = config.MONGODB_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
-db.on('connected', () => console.log('Db connected'));
+db.on('connected', () => console.log('MongoDb connected'));
 db.on('error', console.error.bind(console, 'mongo connection error'));
