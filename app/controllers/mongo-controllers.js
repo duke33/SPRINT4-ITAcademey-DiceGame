3cfed16962408200a3fc592e8923cc120862aa47
Mariano <mariano_farace@hotmail.com>
2021-12-08 19:37:29 +0100

    Refactor to implement controllers

---
+++ app/controllers/mongo-controllers.js
@@ -0,0 +1,107 @@
+/* eslint-disable max-len */
+const express = require('express');
+const User = require('../user-schema');
+const rollTheDice = require('../game');
+// TODO error handling logic!!
+// TODO separar todo en modulos
+// TODO fijate si necesitas .exec!!!!!!!
+// TODO poner status code????
+
+// Crea un jugador
+// TODO hacer que no se pueda repetir el nombre, a no ser que sea anonimo
+const createUser = async (req, res) => {
+  const newPlayer = new User({
+    name: req.body.name,
+  });
+
+  try {
+    await newPlayer.save();
+    res.send(newPlayer);
+  } catch (err) { res.send(err); }
+};
+
+// Modifica el nombre del jugador
+const modifyPlayerName = async (req, res) => {
+  const { playerId } = req.params;
+  const { newName } = req.body;
+  try {
+    const updatedPlayer = await User.findByIdAndUpdate(playerId, { name: newName }, { new: true });
+    res.send(updatedPlayer);
+  } catch (err) { res.send(err); }
+};
+
+// Elimina las tiradas del jugador //TODO cambiar lo que devuelve
+const cleanGameLog = async (req, res) => {
+  const { playerId } = req.params;
+  try {
+    const foundUser = await User.findById(playerId).exec();
+    foundUser.gameLog = [];
+    // TODO ver si esto lo pones como undefined
+    foundUser.successRate = 0;
+    await foundUser.save();
+    res.send(foundUser);
+  } catch (err) { res.send(err); }
+};
+
+// Un jugador específico realiza un tirón
+
+const makeAPlay = async (req, res) => {
+  const { playerId } = req.params;
+
+  const result = rollTheDice();
+  try {
+    const foundPlayer = await User.findById(playerId).exec();
+    foundPlayer.gameLog.push(result);
+    foundPlayer.successRate = foundPlayer.successRateCalc();
+    await foundPlayer.save();
+    res.send({ result });
+  } catch (err) { res.send(err); }
+};
+
+// Devuelve el listado de jugadas por un jugador.
+const playersList = async (req, res) => {
+  const { playerId } = req.params;
+  try {
+    const foundPlayer = await User.findById(playerId).exec();
+    res.send(foundPlayer.gameLog);
+  } catch (err) { res.send(err); }
+};
+
+// Devuelve el porcentaje de exito de un jugardor especifico
+const individualPlayerSuccessRate = async (req, res) => {
+  const { playerId } = req.params;
+  try {
+    const foundPlayer = await User.findById(playerId).exec();
+    res.json(foundPlayer.successRate);
+  } catch (err) { res.send(err); }
+};
+
+// Devuelve el listado de todos los jugadores del sistema con su porcentaje medio de logros
+const playersAndSuccessRateList = async (req, res) => {
+  try {
+    const allPlayers = await User.find({}, 'name successRate').exec();
+    res.json(allPlayers);
+  } catch (err) { res.send(err); }
+};
+
+// GET /players/ranking/winner: devuelve al jugador con mejor porcentaje de éxito
+const winner = async (req, res) => {
+  try {
+    const winnerPlayer = await User.findOne({}).sort('-successRate').exec();
+    res.json(winnerPlayer);
+  } catch (err) { res.send(err); }
+};
+
+// GET /players/ranking/loser: devuelve al jugador con peor porcentaje de éxito
+const looser = async (req, res) => {
+  try {
+    const winnerPlayer = await User.findOne({}).sort('successRate').exec();
+    res.json(winnerPlayer);
+  } catch (err) { res.send(err); }
+};
+
+// TODO GET /players/ranking: devuelve el porcentaje medio de logros del conjunto de todos los jugadores
+
+module.exports = {
+  createUser, modifyPlayerName, cleanGameLog, makeAPlay, playersList, individualPlayerSuccessRate, playersAndSuccessRateList, winner, looser,
+};
--- app/routes/player-router.js
+++ app/routes/player-router.js
@@ -2,8 +2,9 @@
 const express = require('express');
 
 const router = express.Router();
-const User = require('../user-schema');
-const rollTheDice = require('../game');
+const {
+  createUser, modifyPlayerName, cleanGameLog, makeAPlay, playersList, individualPlayerSuccessRate, playersAndSuccessRateList, winner, looser,
+} = require('../controllers/mongo-controllers');
 // TODO error handling logic!!
 // TODO separar todo en modulos
 // TODO fijate si necesitas .exec!!!!!!!
@@ -11,96 +12,32 @@ const rollTheDice = require('../game');
 
 // POST /players: crea un jugador
 // TODO hacer que no se pueda repetir el nombre, a no ser que sea anonimo
-router.post('/', async (req, res) => {
-  const newPlayer = new User({
-    name: req.body.name,
-  });
-
-  try {
-    await newPlayer.save();
-    res.send(newPlayer);
-  } catch (err) { res.send(err); }
-});
+router.post('/', createUser);
 
 // PUT /players: modifica el nombre del jugador
-router.put('/:playerId', async (req, res) => {
-  const { playerId } = req.params;
-  const { newName } = req.body;
-  try {
-    const updatedPlayer = await User.findByIdAndUpdate(playerId, { name: newName }, { new: true });
-    res.send(updatedPlayer);
-  } catch (err) { res.send(err); }
-});
+router.put('/:playerId', modifyPlayerName);
 
 // DELETE /players/{id}/games: elimina las tiradas del jugador //TODO cambiar lo que devuelve
-router.delete('/:playerId/games', async (req, res) => {
-  const { playerId } = req.params;
-  try {
-    const foundUser = await User.findById(playerId).exec();
-    foundUser.gameLog = [];
-    // TODO ver si esto lo pones como undefined
-    foundUser.successRate = 0;
-    await foundUser.save();
-    res.send(foundUser);
-  } catch (err) { res.send(err); }
-});
+router.delete('/:playerId/games', cleanGameLog);
 
 // POST /players/{id}/games: un jugador específico realiza un tirón
 
-router.post('/:playerId/games', async (req, res) => {
-  const { playerId } = req.params;
-
-  const result = rollTheDice();
-  try {
-    const foundPlayer = await User.findById(playerId).exec();
-    foundPlayer.gameLog.push(result);
-    foundPlayer.successRate = foundPlayer.successRateCalc();
-    await foundPlayer.save();
-    res.send({ result });
-  } catch (err) { res.send(err); }
-});
+router.post('/:playerId/games', makeAPlay);
 
 // GET /players/{id}/games: devuelve el listado de jugadas por un jugador.
-router.get('/:playerId/games', async (req, res) => {
-  const { playerId } = req.params;
-  try {
-    const foundPlayer = await User.findById(playerId).exec();
-    res.send(foundPlayer.gameLog);
-  } catch (err) { res.send(err); }
-});
+router.get('/:playerId/games', playersList);
 
 // Devuelve el porcentaje de exito de un jugardor especifico
-router.get('/:playerId/games/rate', async (req, res) => {
-  const { playerId } = req.params;
-  try {
-    const foundPlayer = await User.findById(playerId).exec();
-    res.json(foundPlayer.successRate);
-  } catch (err) { res.send(err); }
-});
+router.get('/:playerId/games/rate', individualPlayerSuccessRate);
 
 // GET /players: devuelve el listado de todos los jugadores del sistema con su porcentaje medio de logros
-router.get('/', async (req, res) => {
-  try {
-    const allPlayers = await User.find({}, 'name successRate').exec();
-    res.json(allPlayers);
-  } catch (err) { res.send(err); }
-});
+router.get('/', playersAndSuccessRateList);
 
 // GET /players/ranking/winner: devuelve al jugador con mejor porcentaje de éxito
-router.get('/ranking/winner', async (req, res) => {
-  try {
-    const winnerPlayer = await User.findOne({}).sort('-successRate').exec();
-    res.json(winnerPlayer);
-  } catch (err) { res.send(err); }
-});
+router.get('/ranking/winner', winner);
 
 // GET /players/ranking/loser: devuelve al jugador con peor porcentaje de éxito
-router.get('/ranking/loser', async (req, res) => {
-  try {
-    const winnerPlayer = await User.findOne({}).sort('successRate').exec();
-    res.json(winnerPlayer);
-  } catch (err) { res.send(err); }
-});
+router.get('/ranking/loser', looser);
 
 // TODO GET /players/ranking: devuelve el porcentaje medio de logros del conjunto de todos los jugadores
 
