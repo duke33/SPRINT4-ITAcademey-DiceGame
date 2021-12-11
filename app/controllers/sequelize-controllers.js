43df31f8d12e2bdd0e399e58f37e946d22b418ca
Mariano <mariano_farace@hotmail.com>
2021-12-11 23:35:31 +0100

    Implement create and update user first implementation

--- app/controllers/sequelize-controllers.js
+++ app/controllers/sequelize-controllers.js
@@ -1,38 +1,49 @@
 /* eslint-disable max-len */
-const User = require('../schemas/user-schema-mongo');
+const User = require('../schemas/user-model-mysql');
 const rollTheDice = require('../helpers/game');
 
 // Crea un jugador
 const createUser = async (req, res, next) => {
   const { name } = req.body;
   try {
-    const previousUser = await User.find({ name });
+    // Un workaround, porque mysql no puede hacer busquedas con where undefined
+    const previousUser = (name === undefined) ? [] : await User.findAll({
+      where: {
+        name,
+      },
+
+    });
 
     if (!(previousUser.length === 0) && !(name === undefined)) { res.status(409).send({ error: 'Duplicated Username' }); } else {
-      const newPlayer = new User({
+      const newUser = await User.create({
         name,
       });
-
-      await newPlayer.save();
-      res.send(newPlayer);
+      res.json(newUser);
     }
   } catch (err) { next(err); }
 };
 
-// Modifica el nombre del jugador
+// Modifica el nombre del jugador //TODO fijate que esto te deja modificar el nombre a uno repetido y eso no deberia pasar! modificarlo tmb en mongo!
+// TODO modificar lo que devuelve
 const modifyPlayerName = async (req, res, next) => {
   const { playerId } = req.params;
   const { newName } = req.body;
   try {
-    const updatedPlayer = await User.findByIdAndUpdate(playerId, { name: newName }, { new: true });
+    const updatedPlayer = await await User.update({ name: newName }, {
+      where: {
+        id: playerId,
+      },
+
+    });
+
     if (!updatedPlayer) {
       throw new Error('User not found');
     }
     res.send(updatedPlayer);
-  } catch (err) { next(err); }
+  } catch (err) { console.log(err); }
 };
 
-// Elimina las tiradas del jugador
+// Elimina las tiradas del jugador //TODO
 const cleanGameLog = async (req, res, next) => {
   const { playerId } = req.params;
   try {
@@ -47,7 +58,7 @@ const cleanGameLog = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 
-// Un jugador específico realiza un tirón
+// Un jugador específico realiza un tirón //TODO
 
 const makeAPlay = async (req, res, next) => {
   const { playerId } = req.params;
@@ -65,7 +76,7 @@ const makeAPlay = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 
-// Devuelve el listado de jugadas por un jugador.
+// Devuelve el listado de jugadas por un jugador. //TODO
 const playersList = async (req, res, next) => {
   const { playerId } = req.params;
   try {
@@ -77,7 +88,7 @@ const playersList = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 
-// Devuelve el porcentaje de exito de un jugardor especifico
+// Devuelve el porcentaje de exito de un jugardor especifico //TODO
 const individualPlayerSuccessRate = async (req, res, next) => {
   const { playerId } = req.params;
   try {
@@ -89,15 +100,18 @@ const individualPlayerSuccessRate = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 
-// Devuelve el listado de todos los jugadores del sistema con su porcentaje medio de logros
+// Devuelve el listado de todos los jugadores del sistema con su porcentaje medio de logros //TODO
 const playersAndSuccessRateList = async (req, res, next) => {
   try {
-    const allPlayers = await User.find({}, 'name successRate');
-    res.json(allPlayers);
+    User.findAll().then((users) => {
+      res.json(users);
+    });
+    // const allPlayers = await User.find({}, 'name successRate');
+    // res.json(allPlayers);
   } catch (err) { next(err); }
 };
 
-// GET /players/ranking/winner: devuelve al jugador con mejor porcentaje de éxito
+// GET /players/ranking/winner: devuelve al jugador con mejor porcentaje de éxito //TODO
 const winner = async (req, res, next) => {
   try {
     const winnerPlayer = await User.findOne({}).sort('-successRate');
@@ -105,7 +119,7 @@ const winner = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 
-// GET /players/ranking/loser: devuelve al jugador con peor porcentaje de éxito
+// GET /players/ranking/loser: devuelve al jugador con peor porcentaje de éxito //TODO
 const looser = async (req, res, next) => {
   try {
     const winnerPlayer = await User.findOne({ successRate: { $ne: null } }).sort('successRate');
@@ -113,7 +127,7 @@ const looser = async (req, res, next) => {
   } catch (err) { next(err); }
 };
 
-// GET /players/ranking: devuelve el porcentaje medio de logros del conjunto de todos los jugadores
+// GET /players/ranking: devuelve el porcentaje medio de logros del conjunto de todos los jugadores //TODO
 const successRateAvg = async (req, res, next) => {
   try {
     const globalAvgSuccessRate = await User.aggregate(
