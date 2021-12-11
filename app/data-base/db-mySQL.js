465d6ec1e2c71d685b2a06f770cb085bfc4f5a79
Mariano <mariano_farace@hotmail.com>
2021-12-11 16:09:02 +0100

    Will create the table if it does not exist

--- app/data-base/db-mySQL.js
+++ app/data-base/db-mySQL.js
@@ -19,7 +19,7 @@ const sequelize = new Sequelize(database, username, password, {
 // TODO Modificarlo para que cree la base de datos si no existe
 const connectSequelize = async () => {
   try {
-    await sequelize.authenticate();
+    await sequelize.sync({ force: false });
     console.log('Connection to mySQL-DB has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
