45e3089a808f9126d6eb555b09fcbe751a356dda
Mariano <mariano_farace@hotmail.com>
2021-12-11 23:34:32 +0100

    Improve user model

--- app/schemas/user-model-mysql.js
+++ app/schemas/user-model-mysql.js
@@ -3,18 +3,11 @@ const sequelize = require('../data-base/db-mySQL');
 
 class User extends Model {}
 User.init({
-  username: DataTypes.STRING,
-  birthday: DataTypes.DATE,
+  name: {
+    type: DataTypes.STRING,
+    defaultValue: 'anonymous',
+  },
+  registration_date: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
 }, { sequelize, modelName: 'user' });
 
 module.exports = User;
-
-// TODO borrar esto
-// (async () => {
-//   await sequelize.sync();
-//   const jane = await User.create({
-//     username: 'janedoe',
-//     birthday: new Date(1980, 6, 20),
-//   });
-//   console.log(jane.toJSON());
-// })();
