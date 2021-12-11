b45882f46580aef6509943a1e7518c13faae6c1b
Mariano <mariano_farace@hotmail.com>
2021-12-11 23:34:49 +0100

    Add comment

--- app/data-base/db-mySQL.js
+++ app/data-base/db-mySQL.js
@@ -19,7 +19,7 @@ const sequelize = new Sequelize(database, username, password, {
 // TODO Modificarlo para que cree la base de datos si no existe
 const connectSequelize = async () => {
   try {
-    await sequelize.sync({ force: false });
+    await sequelize.sync({ force: false });// TODO cambiar a false cuand otermines los modelos!
     console.log('Connection to mySQL-DB has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
