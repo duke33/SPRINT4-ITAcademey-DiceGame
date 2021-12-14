a72883d3664d96af1b8fc00eb3e33cd4314c209a
Mariano <mariano_farace@hotmail.com>
2021-12-14 16:08:35 +0100

    Improve error handling and logging

--- app/middleware/authenticate-jwt.js
+++ app/middleware/authenticate-jwt.js
@@ -8,21 +8,14 @@ const getTokenFrom = (request) => {
   }
   return null;
 };
-// TODO call next!!!!!!
-// TODO borrar los logs
+
 authenticationRouter.all('*', async (req, res, next) => {
-  console.log('Entra al authenticationRouter');
   const token = getTokenFrom(req);
-  console.log('token:::', token);
-  const decodedToken = jwt.verify(token, process.env.SECRET);
-  if (!token || !decodedToken.user) {
-    return res.status(401).json({ error: 'token missing or invalid' });
-  }
-  console.log(`Authenticated as ${decodedToken.user}`)(next());
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    console.log(`Authenticated as ${decodedToken.username}`);
+  } catch (err) { next(err); }
+  next();
 });
 
 module.exports = authenticationRouter;
-// TODO verificar que mierda esta pasando con el usuario aca, parece que no estas controlando nada
-
-//    "token": "eyJhbGciOiJIUzI1NiJ9.YWRtaW4.va0_hFdSaYPdMwhAGumoXQ7RMvIx6hq60kPWCSUOTkI",
-//    "token": "eyJhbGciOiJIUzI1NiJ9.YWRtaW4.va0_hFdSaYPdMwhAGumoXQ7RMvIx6hq60kPWCSUOTkI",
